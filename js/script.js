c76bc5c456f94143af51600584e5cac65bd64c73
sashapoliti <sas.politi@gmail.com>
2024-03-14 15:16:30 +0100

    fail 1

--- js/script.js
+++ js/script.js
@@ -5,8 +5,19 @@ Creare una funzione per capire se la parola inserita è palindroma */
 const userWord = prompt('Inserisci una parola');
 
 palindrome(userWord);
+console.log(palindrome(userWord));
 
 function palindrome(word) {
     const array = userWord.split(""); /* split word in an array */
-    console.log(array);
+    //console.log(array);
+    const reverseArray = array.reverse(); /* create a reverse array */
+    //console.log(reverseArray);
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        const reverseElement = reverseArray[i];
+        if (element !== reverseElement) {
+            return true;
+        }
+    } /* cycle with if to check two arrays */
+    return false;
 }
